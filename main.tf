e8f6a98498ab75428729f3fd53fd40ac4fdbecde
Joaquin Graziano <jgraziano@altec.com.ar>
2023-03-07 13:19:25 -0300

    pruebo ec2

--- main.tf
+++ main.tf
@@ -1,50 +1,6 @@
+#creo ec2 para probar 
 
-
-//se creara
-
-
-
-resource "aws_security_group" "eks_worker_sg" {
-  name_prefix = "eks-worker-"
-
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "eks-worker-sg"
-  }
-}
-
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "my-eks-cluster"
-##  role_arn = "arn:aws:iam::123456789012:role/eks-service-role"
-##replace the 123456789012 with your account number
-  vpc_config {
-    subnet_ids = [
-      aws_subnet.eks_subnet_private.id,
-      aws_subnet.eks_subnet_public.id
-    ]
-
-    security_group_ids = [aws_security_group.eks_worker_sg.id]
-  }
-
-  depends_on = [
-    aws_vpc.eks_vpc,
-    aws_subnet.eks_subnet_private,
-    aws_subnet.eks_subnet_public,
-    aws_internet_gateway.eks_igw,
-    aws_route_table.eks_private_route_table,
-    aws_security_group.eks_worker_sg
-  ]
+resource "aws_instance" "example" {
+	ami = "ami-0357d42faf6fa582f"
+	instance_type = "t2.micro"
 }
