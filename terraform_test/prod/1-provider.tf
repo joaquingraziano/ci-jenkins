4a529cd08130d03f8bdda43efa2578cef2814fa9
jiolloker <jiolloker@hotmail.com>
2023-03-13 16:44:24 -0300

    pushing working prod cluster deployment

--- terraform_test/prod/1-provider.tf
+++ terraform_test/prod/1-provider.tf
@@ -2,5 +2,5 @@ provider "aws" {
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "profile_name"
-  region     = "us-east-1"
+  region                   = "us-east-1"
 }
--- terraform_test/prod/4-eks.tf
+++ terraform_test/prod/4-eks.tf
@@ -1,9 +1,9 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0.0"
+  version = "~> 19.0"
 
   cluster_name    = "eks-webdemo"
-  cluster_version = "1.25"
+  cluster_version = "1.24"
 
   vpc_id                          = module.vpc.vpc_id
   subnet_ids                      = module.vpc.private_subnets
@@ -18,9 +18,6 @@ module "eks" {
       most_recent = true
     }
     vpc-cni = {
-      most_recent = true
-    }
-    csi = {
       most_recent = true
     }
   }
