23bcdf34e5e3130af5b8ae810b25a4eefad41621
jiolloker <jiolloker@hotmail.com>
2023-03-17 09:34:19 -0300

    updated user_data for jenkins install

--- terraform_test/sprint2-infra-var/dev/2-ec2-jenkins.tf
+++ terraform_test/sprint2-infra-var/dev/2-ec2-jenkins.tf
@@ -17,23 +17,19 @@ module "ec2_instance" {
   ]
   ## aqui poner el userdata para instalar jenkins
   user_data = <<-EOF
-  #!/bin/bash -ex
-  sudo apt-get update
-  sudo apt-get install \
-  ca-certificates \
-  curl \
-  gnupg \
-  lsb-release
-  sudo mkdir -m 0755 -p /etc/apt/keyrings
+  #!/bin/bash
+  apt-get update -y
+  apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
+  mkdir -m 0755 -p /etc/apt/keyrings 
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo chmod a+r /etc/apt/keyrings/docker.gpg
-  sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-  sudo systemctl enable docker.service
-  sudo systemctl enable containerd.service
+  chmod a+r /etc/apt/keyrings/docker.gpg
+  apt-get update -y
+  apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+  systemctl enable docker.service
+  systemctl enable containerd.service
   docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts-jdk11
   EOF
 
