1ab81c533c751d047e463dd1f3599623248edee3
jiolloker <jiolloker@hotmail.com>
2023-03-19 15:18:38 -0300

    modified userdata

--- terraform_test/sprint2-infra-var/jenkins/2-ec2-jenkins.tf
+++ terraform_test/sprint2-infra-var/jenkins/2-ec2-jenkins.tf
@@ -21,25 +21,22 @@ module "ec2_instance" {
   ## aqui poner el userdata para instalar jenkins
   user_data = <<EOF
   #!/bin/bash
-  sudo apt-get update -y
-  sudo apt-get install -y \
-  ca-certificates \
-  curl \
-  gnupg \
-  lsb-release
-  sudo mkdir -m 0755 -p /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo chmod a+r /etc/apt/keyrings/docker.gpg
-  sudo apt-get update -y
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-  sudo systemctl enable docker.service
-  sudo systemctl enable containerd.service
-  docker run -d -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts-jdk11
+  sudo apt update -y
+  sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+  apt-cache policy docker-ce
+  sudo apt install docker-ce -y
+  sudo systemctl enable docker
+  sudo systemctl start docker
+  sudo usermod -aG docker ubuntu
+  sudo docker run -d -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts-jdk11
   EOF
-
+  /*
+  access docker container logs for jenkins
+  sudo docker ps
+  sudo docker logs -f <container id>
+  */
   tags = {
     "Terraform"     = "true"
     "Environment" = var.ec2_environment
